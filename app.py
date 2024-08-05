07e189a5fb543bb1174ed0f73f15768433cdfc16
EdsonMichell <118852035+EdsonMichell@users.noreply.github.com>
2024-08-05 12:05:22 -0400

    app.py

--- app.py
+++ app.py
@@ -174,38 +174,39 @@ if check_password():
                 st.success("Produto atualizado com sucesso!")
 
     elif choice == "Registrar Venda":
-        st.subheader("Registrar Venda de Produto")
-        if st.session_state.products['Quantidade'].sum() == 0:
-            st.warning("Estoque está vazio. Cadastre mais produtos.")
-        else:
-            with st.form(key='sales_form'):
-                produto = st.selectbox("Produto", st.session_state.products['Produto'])
-                quantidade_venda = st.number_input("Quantidade", min_value=1, step=1)
-                cliente = st.text_input("Nome do Cliente")
-                forma_pagamento = st.selectbox("Forma de Pagamento", ["Cartão", "Pix", "Dinheiro à vista", "Parcelado"])
-                parcelas = st.number_input("Parcelas", min_value=1, step=1) if forma_pagamento == "Parcelado" else 1
-                conta_recebimento = st.selectbox("Conta para Recebimento", st.session_state.accounts['Conta'].unique())
-                submit_button = st.form_submit_button(label="Registrar Venda")
+    st.subheader("Registrar Venda de Produto")
+    if st.session_state.products['Quantidade'].sum() == 0:
+        st.warning("Estoque está vazio. Cadastre mais produtos.")
+    else:
+        with st.form(key='sales_form'):
+            produto = st.selectbox("Produto", st.session_state.products['Produto'])
+            quantidade_venda = st.number_input("Quantidade", min_value=1, step=1)
+            cliente = st.text_input("Nome do Cliente")
+            forma_pagamento = st.selectbox("Forma de Pagamento", ["Cartão", "Pix", "Dinheiro à vista", "Parcelado"])
+            parcelas = st.number_input("Parcelas", min_value=1, step=1) if forma_pagamento == "Parcelado" else 1
+            conta_recebimento = st.selectbox("Conta para Recebimento", st.session_state.accounts['Conta'].unique())
+            submit_button = st.form_submit_button(label="Registrar Venda")
 
-                if submit_button:
-                    produto_info = st.session_state.products[st.session_state.products['Produto'] == produto].iloc[0]
-                    if produto_info['Quantidade'] >= quantidade_venda:
-                        preco_venda = produto_info['Preço de Venda']
-                        nova_quantidade = produto_info['Quantidade'] - quantidade_venda
-                        st.session_state.products.loc[st.session_state.products['Produto'] == produto, 'Quantidade'] = nova_quantidade
+            if submit_button:
+                produto_info = st.session_state.products[st.session_state.products['Produto'] == produto].iloc[0]
+                if produto_info['Quantidade'] >= quantidade_venda:
+                    preco_venda = produto_info['Preço de Venda']
+                    nova_quantidade = produto_info['Quantidade'] - quantidade_venda
+                    st.session_state.products.loc[st.session_state.products['Produto'] == produto, 'Quantidade'] = nova_quantidade
 
-                        new_sale = pd.DataFrame([[produto, quantidade_venda, preco_venda, cliente, forma_pagamento, parcelas, conta_recebimento]],
-                                                columns=['Produto', 'Quantidade', 'Preço de Venda', 'Cliente', 'Forma de Pagamento', 'Parcelas', 'Conta'])
-                        st.session_state.sales = pd.concat([st.session_state.sales, new_sale], ignore_index=True)
-                        atualizar_saldo_conta(conta_recebimento, preco_venda * quantidade_venda)
-                        save_data(st.session_state.sales, sales_file)
-                        save_data(st.session_state.products, product_file)
-                        st.success("Venda registrada com sucesso!")
+                    new_sale = pd.DataFrame([[produto, quantidade_venda, preco_venda, cliente, forma_pagamento, parcelas, conta_recebimento]],
+                                            columns=['Produto', 'Quantidade', 'Preço de Venda', 'Cliente', 'Forma de Pagamento', 'Parcelas', 'Conta'])
+                    st.session_state.sales = pd.concat([st.session_state.sales, new_sale], ignore_index=True)
+                    atualizar_saldo_conta(conta_recebimento, preco_venda * quantidade_venda)
+                    save_data(st.session_state.sales, sales_file)
+                    save_data(st.session_state.products, product_file)
+                    st.success("Venda registrada com sucesso!")
 
-                        if forma_pagamento == "Pix":
-                            st.image("images/pix.jpg", caption="Pagamento via Pix")
-                    else:
-                        st.error("Quantidade insuficiente em estoque!")
+                    if forma_pagamento == "Pix":
+                        st.image("images/pix.jpg", caption="Pagamento via Pix")
+                else:
+                    st.error("Quantidade insuficiente em estoque!")
+
 
     elif choice == "Relatórios":
         st.subheader("Relatórios Detalhados")
